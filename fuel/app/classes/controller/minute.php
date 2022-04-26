00d8794c35230a9ad85be61df1a31ba14a40274b
Nagatsuka Yuya <yuya.nagatsuka@hr-cloud.co.jp>
2022-04-26 18:12:21 +0900

    DB操作（挿入・取得・更新・削除）のサンプルコードの記述
    今後、DB操作（データの挿入・取得・更新・削除）を行う際にサンプルとなるコードを記述しました。
    これらは開発の際に参考とするものであり、最終的には削除する予定です。

--- fuel/app/classes/controller/minute.php
+++ fuel/app/classes/controller/minute.php
@@ -34,4 +34,46 @@ class Controller_Minute extends Controller
 		echo '議事録一覧ページ';
 	}
 
+	// DBにデータを挿入する際のサンプルコード
+	public function action_insert()
+	{
+		DB::insert('minutes')->set(array(
+			'user_id' => 1,
+			'title' => '議事録７',
+			'summary' => '議事録７の概要です。',
+			'content' => '議事録７の内容・コンテンツです。',
+		))->execute();
+	}
+
+	// DBからデータを取得する際のサンプルコード
+	public function action_select()
+	{
+		$result = DB::select('*')->from('minutes')->where('id', '>=', '6')->order_by('id', 'desc')->execute()->as_array();
+		echo '<pre>';
+		print_r($result);
+	}
+
+	// DBのデータを更新する際のサンプルコード
+	public function action_update()
+	{
+		// 1つのカラムのデータを変更する場合
+		DB::update('minutes')->value('summary', '議事録７概要')->where('id', '=', '7')->execute();
+
+		// 複数のカラムのデータを変更する場合
+		DB::update('minutes')
+		->set(array(
+			'title' => '議事録７タイトル',
+			'summary' => '議事録７概要',
+			'content' => '議事録７内容',
+		))
+		->where('id', '=', '7')
+		->execute();
+	}
+
+	// DBのデータを削除する際のサンプルコード
+	public function action_delete()
+	{
+		DB::delete('minutes')->where('id', '=', '1')->execute();
+	}
+
 }
